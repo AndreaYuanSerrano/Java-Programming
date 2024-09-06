5ebde9a62666530a00943d67771888cd0d470f8d
AndreaYuanSerrano <andreayuanserrano@gmail.com>
2024-09-06 22:44:29 +0800

    Add files via upload

---
+++ RelationalDemo.java
@@ -0,0 +1,21 @@
+public class RelationalDemo {
+	public static void main(String[] args) {
+		
+	  //local vairiables
+		int x = 24;
+		int y = 50;
+		int z = -2;
+		
+		
+	System.out.println("RELATIONAL OPERATORS");
+		System.out.println( x + "==" + y + ":"+ (x==y));
+		System.out.println( x + "!=" + y + ":"+ (x!=y));
+		
+		System.out.println( z + ">" + y + ":"+ (z>y));
+		System.out.println( z + ">=" + y + ":"+ (z>y));
+		
+		System.out.println( z + "<" + x + ":"+ (z<x));
+		System.out.println( z + "<=" + x + ":"+ (z<x));
+		
+	}
+}
